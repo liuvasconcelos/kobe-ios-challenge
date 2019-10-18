3d5a78d7c144b21fb56fe422a3f9501263aecbe4
liuvasconcelos <livia.vasconcelos@stant.com.br>
2019-10-18 19:58:36 -0300

    develop Add structure for project.
    - Remove main storyboard.
    - Set project to be opened on new view controller class.
    
    Signed-off-by: liuvasconcelos <livia.vasconcelos@stant.com.br>

---
+++ kobe-ios-challenge/Application/TopRatedMovies/TopRatedMoviesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TopRatedMoviesViewController.swift
+//  kobe-ios-challenge
+//
+//  Created by Livia Vasconcelos on 18/10/19.
+//
+
+import UIKit
+
+class TopRatedMoviesViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.view.backgroundColor = .red
+    }
+}
